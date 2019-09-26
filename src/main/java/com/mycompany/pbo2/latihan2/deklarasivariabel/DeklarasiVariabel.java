41c8e84a0feaf70fe09179664491f6efd1d88bd7
StevenDanesswaralay <55594713+StevenDanesswaralay@users.noreply.github.com>
2019-09-26 11:13:54 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan2/deklarasivariabel/DeklarasiVariabel.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author 
+ * NAMA     : Steven Danesswaralay
+ * KELAS    : PBO2
+ * NIM      : 10118078
+ * Deskripsi Program : Deklarasi Variabel
+ */
+public class DeklarasiVariabel {
+    public static void main(String[] args){
+        
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI =3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan Hasil
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi nilai PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+
+    }
+}
